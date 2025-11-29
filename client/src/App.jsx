c3f7a7cf67bdd6263b09f5bfa3b2842298ec79a4
Ali Haider <alihaider@foomotion.io>
2025-11-29 09:22:14 +0500

    Setup Clerk auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,21 @@
+import {
+  SignedOut,
+  SignedIn,
+  SignInButton,
+  UserButton,
+} from "@clerk/clerk-react";
+
 function App() {
   return (
     <div>
+      <header>
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
       <h1>Quick.ai - Full Stack AI SaaS</h1>
     </div>
   );
